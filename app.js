bc89d0b5a1db919935bb77f451b2a3cd900b67b1
EC2 Default User <ec2-user@ip-10-0-0-56.ec2.internal>
2020-12-23 04:33:35 +0000

    change1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, This is NGHIA'));
+app.get('/', (req, res) => res.send('Hello World!, This is NGHIA new1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
